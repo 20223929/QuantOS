a9c053a8ec297df8e1c11e05163128d3be1347ec
L <xhlei@foxmail.com>
2026-09-11 12:27:29 +0800

    feat(storage): migrate trade repository to database backend

--- backend/app/storage/repositories/trade_repository.py
+++ backend/app/storage/repositories/trade_repository.py
@@ -1,13 +1,11 @@
 from backend.app.storage.models.trade import TradeRecord
+from .base_repository import BaseRepository
 
 
-class TradeRepository:
-    def __init__(self):
-        self.items = []
+class TradeRepository(BaseRepository):
+    def __init__(self, session):
+        super().__init__(session)
+        self.model = TradeRecord
 
-    def save(self, trade: TradeRecord):
-        self.items.append(trade)
-        return trade
-
-    def all(self):
-        return self.items
+    def save_trade(self, trade: TradeRecord):
+        return self.save(trade)
