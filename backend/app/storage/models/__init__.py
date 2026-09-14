ee26cbf31034e18b68871f60db27ce2dd284d1cc
L <xhlei@foxmail.com>
2026-09-14 18:13:06 +0800

    register execution event projection model

--- backend/app/storage/models/__init__.py
+++ backend/app/storage/models/__init__.py
@@ -1,11 +1,13 @@
 from .base import Base
 from .consumption import ConsumedExecutionEventModel
+from .execution_projection import ExecutionEventProjectionModel
 from .outbox import ExecutionOutboxModel
 from .trading import MarketDataModel, OrderModel, PositionModel, TradeModel
 
 __all__ = [
     "Base",
     "ConsumedExecutionEventModel",
+    "ExecutionEventProjectionModel",
     "ExecutionOutboxModel",
     "MarketDataModel",
     "OrderModel",
